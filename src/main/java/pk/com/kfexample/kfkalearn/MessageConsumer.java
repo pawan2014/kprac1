dc7b4e1af947e9eb3d761919c9914e98e19d7eb8
pk <pawan.gna@outlook.com>
2021-11-13 10:31:06 -0500

    added a new listener for consumerrecord

--- src/main/java/pk/com/kfexample/kfkalearn/MessageConsumer.java
+++ src/main/java/pk/com/kfexample/kfkalearn/MessageConsumer.java
@@ -1,23 +1,27 @@
 package pk.com.kfexample.kfkalearn;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class MessageConsumer {
 	private Logger log = LoggerFactory.getLogger(MessageConsumer.class);
 
-	@KafkaListener(topics = "${myapp.kafka.topic}", 
-			groupId = "xyz",
-			containerFactory = "customKafkaListenerContainerFactory"
-			)
+	@KafkaListener(topics = "${myapp.kafka.topic}", groupId = "xyz", containerFactory = "customKafkaListenerContainerFactory")
 	public void consume(String message) {
 		log.info("MESSAGE recieved -> " + message);
-		// messageRepo.addMessage(message);
+
 	}
-	
-	
+
+	@KafkaListener(topics = {
+			"${myapp.kafka.topic}" }, groupId = "group2", containerFactory = "customKafkaListenerContainerFactory")
+	public void consume(ConsumerRecord<Integer, String> record) {
+		log.info("received = " + record.value() + " with key " + record.key() + " offset=" + record.offset()
+				+ " headers=" + record.headers());
+
+	}
+
 }
