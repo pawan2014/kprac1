b9922644ae7c8c0f80f76c3814eeb37f7b5e6986
pk <pawan.gna@outlook.com>
2021-11-13 11:48:37 -0500

    added a stream configuration and ktable on stream

--- src/main/java/pk/com/kfexample/kfkalearn/Test.java
+++ src/main/java/pk/com/kfexample/kfkalearn/Test.java
@@ -3,9 +3,11 @@ package pk.com.kfexample.kfkalearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 @EnableKafka
+@EnableKafkaStreams 
 public class Test {
 
 	public static void main(String[] args) {
